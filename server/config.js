5c0ececce110ce767d7e24bb592db8f9990c3bb3
Adam Natale <adam.natale@mail.utoronto.ca>
2025-06-18 18:53:02 -0400

    Update to tournament with logs

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const variables = {
     matchPlayToken: "198|XEcARr3RHCOR8N0muykqfuPk2igt3T0X0lgAnirMb9813756",
-    tournamentId: "131218"
+    tournamentId: "196956"
 };
 
 module.exports = { variables };
